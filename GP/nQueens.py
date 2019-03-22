2b3f3d9eae363fb687f37c19b8f3aff48e42d102
dkind <dkind@slb.com>
2019-03-22 12:31:07 +0000

    Add Apache license.

--- GP/nQueens.py
+++ GP/nQueens.py
@@ -1,17 +1,24 @@
-#    This file is part of EAP.
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
 #
-#    EAP is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as
-#    published by the Free Software Foundation, either version 3 of
-#    the License, or (at your option) any later version.
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
-#    EAP is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Lesser General Public License for more details.
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 #
-#    You should have received a copy of the GNU Lesser General Public
-#    License along with EAP. If not, see <http://www.gnu.org/licenses/>.
+
+'''
+<TODO: Description of script function>.
+'''
 
 import operator
 import math
@@ -117,7 +124,11 @@ toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"),
 
 
 def main():
-    random.seed(318)
+    '''
+    <TODO: Main function description>
+    '''
+
+    random.seed(99)
 
     pop = toolbox.population(n=300)
     hof = tools.HallOfFame(1)
@@ -142,9 +153,12 @@ def main():
     print(", ".join(map(str, values)))
 
 # TODO: Need to print out if we've been successful or not.
+# TODO: Need to dump out GP Tree of the HOF (Hall Of Fame)
 
     return pop, log, hof
 
 
 if __name__ == "__main__":
     main()
+
+# EOF

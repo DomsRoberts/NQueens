d0fa14c856f9900af9cdd8e04702a8401cecdc7e
davidfkind <david@kindmail.eu>
2019-03-24 22:59:18 +0000

    Added command line interface: argparse

--- GP/nQueens.py
+++ GP/nQueens.py
@@ -1,25 +1,29 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
+# -*- coding: utf-8 -*-
 #
-#   http://www.apache.org/licenses/LICENSE-2.0
+# Copyright 2019 David Kind
 #
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy
+# of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
 #
 
 '''
 <TODO: Description of script function>.
 '''
 
+import os
+import sys
+import time                     # Used to time script execution.
+import argparse
 import operator
 import math
 import random
@@ -31,6 +35,17 @@ from deap import base
 from deap import creator
 from deap import tools
 from deap import gp
+
+__author__ = 'David Kind'
+__date__ = '24-03-2019'
+__version__ = '1.0'
+__copyright__ = 'http://www.apache.org/licenses/LICENSE-2.0'
+
+#
+# Main script defines
+#
+SCRIPTNAME = os.path.basename(sys.argv[0])
+SCRIPTINFO = "{} version: {}, {}".format(SCRIPTNAME, __version__, __date__)
 
 # TODO: Is there an optimum number of values required to learn the integer
 #  sequence? Test min/max???
@@ -160,6 +175,21 @@ def main():
 
 
 if __name__ == "__main__":
+    start = time.time()        # Used to time script execution.
+    PARSER = argparse.ArgumentParser(description=__doc__,
+                                     version=SCRIPTINFO,
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    PARSER.add_argument('--timer', '-t',
+                        help='Script execution time.',
+                        action='store_true')
+    # Get the arguments dictionary, where arguments are the keys.
+    ARGS = vars(PARSER.parse_args())
+
+    # Start up the application
     main()
+
+    # Are we on the timer?
+    if ARGS['timer']:
+        print("Script execution time:", time.time() - start, "seconds")
 
 # EOF
